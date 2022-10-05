0f22a0a743abe020a815df98a9d63ecdd3242991
hashimirshad <hashimirshad2000@gmail.com>
2022-10-05 07:50:37 +0530

    test

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -2,8 +2,10 @@ from unittest import skip
 from urllib import response 
 from django.test import TestCase,Client
 #test case is django standared libary,client is the function wich will go to browser and check if it works 
+from django.http import HttpRequest 
 from django.urls import reverse
 from django.contrib.auth.models import User
+from store.views import all_products #homepage view
 
 from store.models import Category, Product
 
@@ -13,12 +15,19 @@ class TestViewResponses(TestCase):
         User.objects.create(username='admin')
         Category.objects.create(name='django',slug='django')
         Product.objects.create(category_id=1,title ='django beginners',created_by_id=1,slug='django-beginners',price='100.00',image='django')
+    
     def test_url_allowed_hosts(self):
         #test allowed hosts#homepage
         response = self.c.get('/')
         self.assertEqual(response.status_code,200)
+    
     def test_product_detail_url(self):
         response =self.c.get(reverse('store:product_detail',args=['django-beginners']))
+    
     def test_category_detail_url(self):
         response =self.c.get(reverse('store:category_list',args=['django']))
 
+    def test_homepage_html(self):
+        request = HttpRequest()
+        response = all_products(request)
+        
